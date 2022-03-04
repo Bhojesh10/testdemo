f2e29a9af8e16097970f3a2a59132ea366b6aa86
Bhojesh10 <bhojesh10@gmail.com>
2022-03-03 22:03:43 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var request= new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+request.onload=function(){
+var data=JSON.parse(request.response);
+console.log(data);
+
+let res=data.filter((element)=>element.region==="Asia");
+console.log(res);
+for(var i=0;i<res.length;i++);{
+console.log(res[i].population)
+
+}
+ 
+}
+
